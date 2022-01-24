e7364968231ab1f98422238ae2a4053ca1dd456a
Matthew Barker <mbarker@softwareinstitute.com>
2022-01-24 17:11:42 +0000

    sixth Commit Animal added to main

--- src/main/java/com/TSImatthewAnimal/Main.java
+++ src/main/java/com/TSImatthewAnimal/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
     public static void main(String[] args){
 
-        System.out.println("Hello World!");
+        Koala koala = new Koala();
+
+        koala.setAge(16);
+
+        System.out.println(koala.getAge());
+
 
     }
 }
